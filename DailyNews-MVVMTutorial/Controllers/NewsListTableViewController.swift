d19d5b5130523113502ef97270820f8ac5626d53
damlacim <cim.damla@gmail.com>
2022-03-29 15:28:19 +0300

    add table view controller

---
+++ DailyNews-MVVMTutorial/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsListTableViewController.swift
+//  DailyNews-MVVMTutorial
+//
+//  Created by Damla Çim on 29.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    private func setup() {
+        
+    }
+    
+    
+    
+}
